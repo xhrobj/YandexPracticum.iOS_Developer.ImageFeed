1cea5cee2ca4ef0acfecf5013593e6033d223a8a
M <m1@mishe1.ru>
2024-01-11 21:57:55 +0300

    Add Table DataSource & Delegate

--- ImageFeed/ImagesListViewController.swift
+++ ImageFeed/ImagesListViewController.swift
@@ -8,6 +8,9 @@
 import UIKit
 
 final class ImagesListViewController: UIViewController {
+    let cellIdentifier = "cell"
+    
+    // MARK: - @IBOutlets
 
     @IBOutlet private var tableView: UITableView!
     
@@ -15,7 +18,35 @@ final class ImagesListViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureTableView()
+    }
+}
+
+// MARK: - <UITableViewDataSource>
+
+extension ImagesListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
     }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        UITableViewCell()
+    }
+}
+
+// MARK: - <UITableViewDelegate>
+
+extension ImagesListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
+// MARK: - Private methods
+
+private extension ImagesListViewController {
+    func configureTableView() {
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
 }
