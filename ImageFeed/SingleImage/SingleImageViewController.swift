3089708d64078d5ede3b8ea0a96af390afaa3033
M <m1@mishe1.ru>
2024-10-30 22:10:52 +0300

    Add SingleImageViewController

---
+++ ImageFeed/SingleImage/SingleImageViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Mikhail Eliseev on 30.10.2024.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    
+    // MARK: - @IBOutlets
+    
+    @IBOutlet private var imageView: UIImageView!
+}
