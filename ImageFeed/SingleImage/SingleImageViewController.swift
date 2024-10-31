af01be27c5b093f12fecf1fefd4ab15570a52912
M <m1@mishe1.ru>
2024-10-31 21:17:58 +0300

    Add Segue Improved

--- ImageFeed/SingleImage/SingleImageViewController.swift
+++ ImageFeed/SingleImage/SingleImageViewController.swift
@@ -8,8 +8,17 @@
 import UIKit
 
 final class SingleImageViewController: UIViewController {
+    var image: UIImage?
     
     // MARK: - @IBOutlets
     
     @IBOutlet private var imageView: UIImageView!
+    
+    // MARK: - View lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imageView.image = image
+    }    
 }
