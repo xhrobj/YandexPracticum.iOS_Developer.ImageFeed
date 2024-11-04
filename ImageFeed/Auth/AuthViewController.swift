e377a72ac23f3b566a3296f55dda693e829b9146
M <m1@mishe1.ru>
2024-11-04 15:39:26 +0300

    Add WebViewController

--- ImageFeed/Auth/AuthViewController.swift
+++ ImageFeed/Auth/AuthViewController.swift
@@ -13,4 +13,23 @@ final class AuthViewController: UIViewController {
     
     @IBAction private func loginButtonTapped() {
     }
+    
+    // MARK: - View lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureNavBackButton()
+    }
+}
+
+// MARK: - Private methods
+
+private extension AuthViewController {
+    func configureNavBackButton() {
+        navigationController?.navigationBar.backIndicatorImage = UIImage(named: "nav_back")
+        navigationController?.navigationBar.backIndicatorTransitionMaskImage = UIImage(named: "nav_back")
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        navigationItem.backBarButtonItem?.tintColor = UIColor(named: "YP Black")
+    }
 }
---
+++ ImageFeed/Auth/WebViewController.swift
@@ -0,0 +1,15 @@
+//
+//  WebViewController.swift
+//  ImageFeed
+//
+//  Created by Mikhail Eliseev on 04.11.2024.
+//
+
+import UIKit
+import WebKit
+
+final class WebViewController: UIViewController {
+    
+    
+    @IBOutlet private var webView: WKWebView!
+}
