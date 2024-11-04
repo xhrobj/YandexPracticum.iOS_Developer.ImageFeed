9973916c54ea7686f783c2a78020b5ad79b9231b
M <m1@mishe1.ru>
2024-11-04 13:20:00 +0300

    Add Constants

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by Mikhail Eliseev on 04.11.2024.
+//
+
+import Foundation
+
+enum Constants {
+    static let accessKey = "d5qsq6-WZUnGNoqkCuBT6bUrdh6crNangyZaIn9rkt0"
+    static let secretKey = "XFjoObaqZDGTzG6rBEYseZg5-IEJwfEM_ShrUKRanws"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+}
