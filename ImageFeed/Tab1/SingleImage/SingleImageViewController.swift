b077e1772df9066f5f6b0c86537c315f09167962
M <m1@mishe1.ru>
2024-11-14 22:47:03 +0300

    Add showError() to SingleImageVC

--- ImageFeed/Tab1/SingleImage/SingleImageViewController.swift
+++ ImageFeed/Tab1/SingleImage/SingleImageViewController.swift
@@ -23,18 +23,20 @@ final class SingleImageViewController: UIViewController {
     
     @IBAction private func shareButtonTapped() {
         guard let image = imageView.image else { return }
-
+        
         let activityViewController = UIActivityViewController(activityItems: [image], applicationActivities: nil)
         present(activityViewController, animated: true, completion: nil)
     }
     
     // MARK: - View lifecycle
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         configureImageScrollView()
         configureImageView()
+        
+        loadAndDisplayImage()
     }
 }
 
@@ -51,45 +53,69 @@ extension SingleImageViewController: UIScrollViewDelegate {
 private extension SingleImageViewController {
     func configureImageScrollView() {
         imageScrollView.delegate = self
-
+        
         imageScrollView.minimumZoomScale = 0.1
         imageScrollView.maximumZoomScale = 1.25
     }
     
     func configureImageView() {
-        guard let imageLink = self.imageLink else { return }
+        self.imageView.contentMode = .scaleAspectFit
+    }
+    
+    func loadAndDisplayImage() {
+        guard let imageLink = self.imageLink, let imageURL = URL(string: imageLink) else { return }
         
-        let placeholder = UIImage(named: "image_placeholder")
-        imageView.kf.setImage(with: URL(string: imageLink), placeholder: placeholder) { [weak self] _ in
+        UIBlockingProgressHUD.show()
+        imageView.kf.setImage(with: imageURL) { [weak self] result in
+            UIBlockingProgressHUD.dismiss()
+            
             guard let self = self else { return }
             
-            imageView.contentMode = .scaleAspectFit
-            imageView.kf.indicatorType = .none
-            
-            guard let image = imageView.image else { return }
-            
-            imageView.frame.size = image.size
-            rescaleAndCenterImageInScrollView(image: image)
+            switch result {
+            case .success(let imageResult):
+                self.imageView.frame.size = imageResult.image.size
+                self.rescaleAndCenterImageInScrollView(image: imageResult.image)
+            case .failure:
+                self.showError()
+            }
         }
     }
-
-    private func rescaleAndCenterImageInScrollView(image: UIImage) {
+    
+    func rescaleAndCenterImageInScrollView(image: UIImage) {
         let minZoomScale = imageScrollView.minimumZoomScale
         let maxZoomScale = imageScrollView.maximumZoomScale
-
+        
         let imageSize = image.size
         let visibleRectSize = imageScrollView.bounds.size
         let hScale = visibleRectSize.width / imageSize.width
         let vScale = visibleRectSize.height / imageSize.height
         let scale = min(maxZoomScale, max(minZoomScale, max(hScale, vScale)))
-
+        
         imageScrollView.setZoomScale(scale, animated: false)
         imageScrollView.layoutIfNeeded()
-
+        
         let newContentSize = imageScrollView.contentSize
         let x = (newContentSize.width - visibleRectSize.width) / 2
         let y = (newContentSize.height - visibleRectSize.height) / 2
-
+        
         imageScrollView.setContentOffset(CGPoint(x: x, y: y), animated: false)
+    }
+    
+    private func showError() {
+        let alertController = UIAlertController(
+            title: "Что-то пошло не так(",
+            message: "Попробовать ещё раз?",
+            preferredStyle: .alert
+        )
+        
+        let cancelAction = UIAlertAction(title: "Не надо", style: .cancel, handler: nil)
+        let retryAction = UIAlertAction(title: "Повторить", style: .default) { [weak self] _ in
+            self?.loadAndDisplayImage()
+        }
+        
+        alertController.addAction(cancelAction)
+        alertController.addAction(retryAction)
+        
+        present(alertController, animated: true)
     }
 }
